701d7b62672372f38c73661f76016bea634a8320
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-01-04 06:09:04 +0530

    Update training.py

--- training.py
+++ training.py
@@ -13,25 +13,39 @@ from tensorflow.keras.callbacks import TensorBoard
 import pickle
 import time
 
-def get_data_x(location):
+def get_data_x_test(location):
     pickle_in = open(location, "rb")
     x = pickle.load(pickle_in)
     return x
 
-def get_data_y(location):
+def get_data_y_test(location):
     pickle_in = open(location, "rb")
     y = pickle.load(pickle_in)
     return y
 
-x = get_data_x("x.pickle")
+def get_data_x_train(location):
+    pickle_in = open(location, "rb")
+    x = pickle.load(pickle_in)
+    return x
+
+def get_data_y_train(location):
+    pickle_in = open(location, "rb")
+    y = pickle.load(pickle_in)
+    return y
+
+X_train = get_data_x_train("x_train.pickle")
 #print(x[1])
-y = get_data_y("y.pickle")
+y_train = get_data_y_train("y_train.pickle")
+
+X_test = get_data_x_test("x_test.pickle")
+#print(x[1])
+y_test = get_data_y_test("y_test.pickle")
 #print(y[1])
 #y_binary = to_categorical(y)
-x = x/255.0
 
-
-            
+X_train = X_train/255.0
+X_test = X_test/255.0
+           
 model = Sequential()
             
 model.add(Conv2D(64,(3,3), input_shape =(60,60,3)))
@@ -43,20 +57,23 @@ model.add(Conv2D(64,(3,3)))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
             
-            
-            
 model.add(Flatten())
-            
-model.add(Dense(64))
+          
+model.add(Dense(16))
 model.add(Activation("relu"))
-            
-model.add(Dense(8))
-model.add(Activation("sigmoid"))
-            
-model.compile(loss="sparse_categorical_crossentropy", optimizer ="adam", metrics=['accuracy'])
-model.fit(x,y,batch_size = 30, epochs=50, validation_split = 0.1)
 
-model.save("ashannew.model")
+model.add(Dense(16))
+model.add(Activation("relu")) 
+
+           
+model.add(Dense(8))
+model.add(Activation("softmax"))
+            
+model.compile(loss="categorical_crossentropy", optimizer ="adam", metrics=['accuracy'])
+model.fit(X_train,y_train,validation_data=(X_test,y_test),batch_size = 64, epochs=5)
+model.evaluate(X_test,y_test)
+#model.save("ashannew1234567.model")
 #score = model.evaluate(x,y, verbose =0)
 #print("Test loss:",score[0])
 #print("Test accuracy:",score[1])
+
